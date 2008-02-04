06f60968022bdb0b3f0cd09529f71ee524f2c0c5
aaronwinborn <aaron@aaronwinborn.com>
2008-02-04 19:21:40 +0000

    begin js add callback for block_refresh/$block/$delta add settings form to block admin forms begin implementation of block refresh

---
+++ js/block_refresh.js
@@ -0,0 +1,23 @@
+// $Id:$
+
+// store the timer and current div data
+_block_refresh_data = new Array();
+
+// this stores all our static data
+function block_refresh_data(timer_delay, url) {
+  this._timer_delay = timer_delay;
+  this._url = url;
+}
+
+// set the timer on or off
+function block_refresh_timer(div) {
+  // stop the current timer
+  clearTimeout(_block_refresh_data[div]._timer_id);
+  _block_refresh_data[div]._timer_id = setTimeout("block_refresh('" + div + "')", _block_refresh_data[div]._timer_delay);
+}
+
+function block_refresh(div) {
+  $(div).load(_block_refresh_data[div]._url);
+  block_refresh_timer(div);
+}
+
