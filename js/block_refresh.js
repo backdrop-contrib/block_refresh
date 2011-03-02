d4528af4060d31448a1b060bb04a03e2d0e34ada
Mike Lander <mlander@elevatedthird.com>
2011-03-02 15:53:44 -0700

    Issue #1017768 by mikegfx: Added args and $_GET values to js refresh.  Additionally, fixed manual refresh permissions.

--- js/block_refresh.js
+++ js/block_refresh.js
@@ -3,9 +3,15 @@
 Drupal.behaviors.qtip = {
   attach: function(context) {
 		$.each(Drupal.settings.block_refresh.settings, function(element, settings) {
-			if (settings['auto']) {
+      //Get argument from referring page and append to end of load request
+      args = '';
+      $.each(Drupal.settings.block_refresh.args, function(index, arg) {
+        args += '/'+arg;
+      });
+      query = Drupal.settings.block_refresh.query;
+     	if (settings['auto']) {
 				setInterval(function() {
-					$('#' + element + ' .content').load(Drupal.settings.basePath + 'block_refresh/' + settings['block']['block'] + '/' + settings['block']['delta']);
+					$('#' + element + ' .content').load(Drupal.settings.basePath + 'block_refresh/' + settings['block']['block'] + '/' + settings['block']['delta'] + args + query);
 				}, settings['timer'] * 1000); // We need to multiply by 1000 because the admin enters a number in seconds,  but the setInterval() function expects milliseconds
 			}
 			if (settings['manual']) {
@@ -23,7 +29,7 @@ Drupal.behaviors.qtip = {
 			
 			$('.block-refresh-button').click(function() {
 				$(this).addClass('block-refresh-button-throbbing');
-				$('#' + element + ' .content').load(Drupal.settings.basePath + 'block_refresh/' + settings['block']['block'] + '/' + settings['block']['delta'], '', function() {
+				$('#' + element + ' .content').load(Drupal.settings.basePath + 'block_refresh/' + settings['block']['block'] + '/' + settings['block']['delta'] + args + query, '', function() {
 					$('.block-refresh-button').removeClass('block-refresh-button-throbbing');
 				});
 			});
