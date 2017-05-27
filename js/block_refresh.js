7f7e30743ebaa265e03a478bee70b6d99916a88b
Jen Lampton <jen@jeneration.com>
2017-05-26 17:18:54 -0700

    Fix block refresh to work with layouts and UUIDs. Update refesh icon and css.

--- js/block_refresh.js
+++ js/block_refresh.js
@@ -13,9 +13,15 @@
         return;
       }
 
-      setBlockRefresh('.' + element, '.block-content', settings['auto'], settings['manual'], settings['init'], settings['timer'], settings['block']['block'], settings['block']['delta']);
+      // Look for the default block class, or the block-refresh class.
+      var element_content = false;
+      if ( $('.block-content').parents('.' + element).length == 1 ) {
+        element_content = '.block-content';
+      }
 
-      function setBlockRefresh(element, element_content, auto, manual, init, timer, block, delta) {
+      setBlockRefresh('.' + element, element_content, settings['auto'], settings['manual'], settings['init'], settings['timer'], settings['module'], settings['delta'], settings['uuid']);
+
+      function setBlockRefresh(element, element_content, auto, manual, init, timer, block, delta, uuid) {
         // Do not bother if no element exists or has already been processed.
         if (!$(element).length || $(element).hasClass('block-refresh-processed')) {
           return;
@@ -26,18 +32,20 @@
         // Always get the argument from the referring page and append the to
         // end of the load request.
         args = '';
-        query = '';
         $.each(Backdrop.settings.block_refresh.args, function (index, arg) {
           args += '/' + arg;
         });
         query = Backdrop.settings.block_refresh.query;
 
         var prefix = Backdrop.settings.block_refresh.cleanUrl ? '' : '?q=';
-        var path = Backdrop.settings.basePath + prefix + Backdrop.settings.pathPrefix + 'block_refresh/' + block + '/' + delta + args + query;
+        var path = Backdrop.settings.basePath + prefix + Backdrop.settings.pathPrefix + 'block_refresh/' + block + '/' + delta + '/' + uuid + args + query;
+
         if (auto && context == document) {
           setInterval(function () {
             BlockRefreshContent(path, element, element_content, manual);
-          }, timer * 1000); // We need to multiply by 1000 because the admin enters a number in seconds,  but the setInterval() function expects milliseconds
+            // We need to multiply by 1000 because the admin enters a number in
+            // seconds, but the setInterval() function expects milliseconds.
+          }, timer * 1000);
         }
         if (init && context == document) {
           BlockRefreshContent(path, element, element_content, manual);
@@ -45,12 +53,17 @@
       }
 
       function addBlockRefreshButton(path, element, element_content, manual) {
-        var refresh_link = '<div class="block-refresh-button">' + Backdrop.t('Refresh') + '</div>';
-        // We'll attach the refresh link to the header if it exists...
+        var refresh_link = '<div class="block-refresh-button">' + Backdrop.t('Refresh') + '<span class="icon"></span></div>';
+
+        if ($(".block-refresh-button")[0]){
+          return;
+        }
+
+        // Attach the refresh link to the header if it exists...
         if ($(element + ' h2').length) {
           $(element + ' h2').before(refresh_link);
         }
-        // ...otherwise we will attach it to the content
+        // ...otherwise we will attach it to the content.
         else {
           $(element + ' ' + element_content).before(refresh_link);
         }
@@ -64,13 +77,19 @@
 
       function BlockRefreshContent(path, element, element_content, manual) {
         $.get(path, function (data) {
-          var contents = $(data).html();
-          // if this is a panel, preserve panel title.
-          var oldh2 = $(element + ' h2.pane-title');
-          $(element).html(contents);
-          //$(element).removeClass('block-refresh-processed');
+          if (element_content) {
+            // Replace what's inside the element_content wrapper element.
+            $(element + ' ' + element_content).html(data);
+          }
+          else {
+            // Otherwise replace everything (Note: this will wipe the title!)
+            $(element).html(data);
+          }
+
           if (manual) {
             addBlockRefreshButton(path, element, element_content, manual);
+            // Remove the throbbing class.
+            $(".block-refresh-button").removeClass('block-refresh-button-throbbing');
           }
           Backdrop.attachBehaviors();
         });
